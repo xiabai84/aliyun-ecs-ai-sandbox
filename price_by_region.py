146fd83a5f7fcb1339b9f8b75892450ae9e919ec
xiabai84@gmail.com <xiabai84@gmail.com>
2020-03-12 00:21:09 +0100

    add utils

---
+++ price_by_region.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+import concurrent.futures
+
+from tabulate import tabulate
+
+import click
+
+from utils import Config, do_action
+
+from aliyunsdkecs.request.v20140526 import DescribeSpotPriceHistoryRequest
+from aliyunsdkecs.request.v20140526 import DescribeRegionsRequest
+from aliyunsdkecs.request.v20140526 import DescribeZonesRequest
+
+
+def query_price(config, region, zone, instance_type):
+    client  = config.create_api_client(region['RegionId'])
+    request = DescribeSpotPriceHistoryRequest.DescribeSpotPriceHistoryRequest()
+    request.set_ZoneId(zone['ZoneId'])
+    request.set_NetworkType('vpc')
+    request.set_InstanceType(instance_type)
+    result = do_action(client, request)
+    ret = []
+    try:
+        item = result['SpotPrices']['SpotPriceType'][-1]
+        ret.append(
+            [
+                zone['LocalName'],
+                instance_type,
+                item['Timestamp'],
+                item['SpotPrice']
+            ]
+        )
+    except IndexError:
+        pass
+    return ret
+
+
+@click.command()
+@click.option(
+    '--max-price', '-m', type=float, default=1000,
+    help="max price (per hour)")
+@click.option(
+    '--type', '-t', default='gn', help="Instance type",
+    type=str)
+    # type=click.Choice(['gn', 'r5', 'se1', 'g5', 'i2']))
+def main(max_price, type):
+    config = Config()
+    config.obtain_secret('access_key_id')
+    config.obtain_secret('access_key_secret')
+
+    click.echo("This query could take a while... Please wait...")
+
+    client  = config.create_api_client('eu-central-1')
+    regions = get_regions(client)
+    futures = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        for region in regions:
+            client  = config.create_api_client(region['RegionId'])
+            zones = get_zones(client)
+            for zone in zones:
+                ins_types = zones[0]['AvailableInstanceTypes']['InstanceTypes']
+                ins_types = [t for t in ins_types if t.startswith('ecs.{}'.format(type))]
+                if 'ecs.gn5-c4g1.xlarge' not in ins_types:
+                    # there is a open issue for DescribeZonesRequest. because the return of AvailableInstanceTypes may loss some candidates
+                    # add common gpu instance type
+                    ins_types.append('ecs.gn5-c4g1.xlarge')
+                for instance_type in ins_types:
+                    # query_price(config, region, zone, instance_type)
+                    future = executor.submit(
+                        query_price,
+                        config, region, zone, instance_type,
+                    )
+
+                    futures.append(future)
+    table = []
+    for future in concurrent.futures.as_completed(futures):
+        try:
+            data = future.result()
+            table.extend(data)
+        except Exception as exc:
+            print('Generated an exception: %s' % (exc))
+
+    table  = [row for row in table if row[-1]< max_price]
+    table.sort(key=lambda x: (x[0], x[-1]))
+    print(tabulate(table))
+
+
+def get_regions(client):
+    request = DescribeRegionsRequest.DescribeRegionsRequest()
+    result = do_action(client, request)
+    return result['Regions']['Region']
+
+def get_zones(client):
+    request = DescribeZonesRequest.DescribeZonesRequest()
+    result = do_action(client, request)
+    return result['Zones']['Zone']
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/security.py
@@ -0,0 +1,99 @@
+import os
+import time
+
+from aliyunsdkecs.request.v20140526 import (
+    CreateSecurityGroupRequest,
+    DescribeSecurityGroupsRequest,
+    AuthorizeSecurityGroupRequest,
+    CreateVpcRequest,
+    CreateVSwitchRequest,
+    DescribeVSwitchesRequest,
+    DescribeKeyPairsRequest,
+    ImportKeyPairRequest,
+)
+
+from .action import do_action
+from .select import BaseConfigParameterSelect
+
+
+class SecurityGroupsSelect(BaseConfigParameterSelect):
+    name = "安全组"
+    key = ['CreateInstanceParams', 'SecurityGroupId']
+    request_cls = DescribeSecurityGroupsRequest.DescribeSecurityGroupsRequest
+    items_getter = lambda self, x: x['SecurityGroups']['SecurityGroup']
+    item_key = "SecurityGroupId"
+    select_item_formatter = lambda self, x: "{}({})".format(x['SecurityGroupName'], x['SecurityGroupId'])
+
+    def handle_selected_item(self, item):
+        self.set_VSwitchId(item['VpcId'])
+
+    def fix_empty_items(self):
+        # 因为创建安全组时需要指定 VpcId, 这里偷个懒， 假定安全组为空时， vpc 和 vswitch
+        # 也为空， 创建一个新的
+        print('Creating VPC please wait ...')
+        self.create_vpc()
+        print('Creating VSwitch please wait ...')
+        time.sleep(5)
+        self.create_vswitch()
+        print('Creating SecurityGroup plase wait ...')
+        self.create_sg()
+        time.sleep(5)
+        self.add_sg_rule()
+
+    def create_sg(self):
+        request = CreateSecurityGroupRequest.CreateSecurityGroupRequest()
+        request.set_VpcId(self.VpcId)
+        result = do_action(self.client, request)
+        self.SecurityGroupId = result['SecurityGroupId']
+
+    def add_sg_rule(self):
+        # Adding SecurityGroup for Jupyter Notebook
+        request = AuthorizeSecurityGroupRequest.AuthorizeSecurityGroupRequest()
+        request.set_IpProtocol("tcp")
+        request.set_PortRange("8880/8888")
+        request.set_SecurityGroupId(self.SecurityGroupId)
+        request.set_SourceCidrIp('0.0.0.0/0')
+
+    def create_vpc(self):
+        request = CreateVpcRequest.CreateVpcRequest()
+        request.set_VpcName('ems-ml-auto-vpc')
+        request.set_CidrBlock('192.168.0.0/16')
+        result = do_action(self.client, request)
+        self.VpcId = result['VpcId']
+
+    def create_vswitch(self):
+        request = CreateVSwitchRequest.CreateVSwitchRequest()
+        request.set_CidrBlock('192.168.0.0/24')
+        request.set_VpcId(self.VpcId)
+        ZoneId = self.config.get(['CreateInstanceParams', 'ZoneId'])
+        request.set_ZoneId(ZoneId)
+        result = do_action(self.client, request)
+
+    def set_VSwitchId(self, vpc_id):
+        request = DescribeVSwitchesRequest.DescribeVSwitchesRequest()
+        request.set_VpcId(self.VpcId)
+        result = do_action(self.client, request)
+        item = result['VSwitches']['VSwitch'][0]
+        self.config.set(['CreateInstanceParams', 'VSwitchId'], item['VSwitchId'])
+
+
+class KeyPairsSelect(BaseConfigParameterSelect):
+    name = "SSH KeyValue pair"
+    key = ['CreateInstanceParams', 'KeyPairName']
+    request_cls = DescribeKeyPairsRequest.DescribeKeyPairsRequest
+    items_getter = lambda self, x: x['KeyPairs']['KeyPair']
+    item_key = "KeyPairName"
+    select_item_formatter = lambda self, x: x['KeyPairName']
+
+    def fix_empty_items(self):
+        self.import_key()
+
+    def import_key(self):
+        request = ImportKeyPairRequest.ImportKeyPairRequest()
+        request.set_KeyPairName('ml-sshkey')
+        key_path = os.path.expanduser('~/.ssh/id_rsa.pub')
+        with open(key_path) as f:
+            keybody = f.read()
+
+        request.set_PublicKeyBody(keybody)
+        do_action(self.client, request)
